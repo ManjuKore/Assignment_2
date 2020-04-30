2b2b44f84d7deb0b24280b3c1207559bcff78f98
ManjuKore <52211576+ManjuKore@users.noreply.github.com>
2020-04-30 17:58:05 +0530

    Delete scripts.sql

--- scripts.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE user (user_id integer primary key,user_Name  varchar(50),user_age integer,department  varchar(50),Subject varchar(50));
-INSERT INTO user (user_id, user_name,user_age,department, Subject) VALUES ('123', 'PRITI', 22, 'CS','Devops');
-INSERT INTO user (user_id, user_name,user_age,department, Subject) VALUES ('101', 'PRIYA', 26, 'CS','ML:');
-
